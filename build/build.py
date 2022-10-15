95fed0e11db7655be3909805705bd255c441df3b
auracreepey <77360773+auracreepey@users.noreply.github.com>
2022-10-15 17:05:46 +0200

    Dernière mise à jour avant release
    Ajout des fichiers de build pour installer facilement les modules python et compiler le launcher.

---
+++ build/build.py
@@ -0,0 +1,7 @@
+#Ce script lance le programme de compilation du launcher.
+#PREREQUIS: -Python (3.10 de préférence)
+#           -Google Chrome
+
+print("Lancement du programme de compilation...")
+print("Assurez-vous d'avoir installé Python 3.10 ET Google Chrome")
+auto-py-to-exe
